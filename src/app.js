8e28bcfd84845dd250032b5cb3dc16e54134c710
vivek chandola <47598844+vivekchandola08@users.noreply.github.com>
2022-07-29 00:40:19 +0530

    Completed File Handling and Routing

--- src/app.js
+++ src/app.js
@@ -10,9 +10,45 @@ app.set('view engine', 'ejs')
 
 app.use(express.static(path.join(__dirname, 'public')))
 
+const accountData = fs.readFileSync(
+    path.join(__dirname, 'json', 'accounts.json'), 'utf8'
+)
+const accounts = JSON.parse(accountData)
+
+const userData = fs.readFileSync(
+    path.join(__dirname, 'json', 'users.json'), 'utf8'
+)
+const users = JSON.parse(userData)
+
 app.get('/',(req,res)=>{
     res.render(
-        'index',{title:'Index'}
+        'index',
+        {title:'Account Summary', accounts}
+    )
+})
+
+app.get('/savings',(req,res)=>{
+    res.render(
+        'account',
+        {account:accounts.savings}
+    )
+})
+app.get('/checking',(req,res)=>{
+    res.render(
+        'account',
+        {account:accounts.checking}
+    )
+})
+app.get('/credit',(req,res)=>{
+    res.render(
+        'account',
+        {account:accounts.credit}
+    )
+})
+app.get('/profile',(req,res)=>{
+    res.render(
+        'profile',
+        {user:users[0]}
     )
 })
 
